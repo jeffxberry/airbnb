b13a20b39d30c2372830f6b4629a55639e91959b
Jeff Berry <jeff.michael.berry@gmail.com>
2026-04-13 12:43:27 -0700

    chore: project scaffold

---
+++ config.py
@@ -0,0 +1,7 @@
+RECEIPTS_DIR      = "/Users/jeffberry/Documents/Rice Drive LLC/SalesReceipts/"
+SOURCE_EMAIL      = "Accounting@myrentaladvisor.com"
+SUBJECT_KEYWORDS  = ["Sales Receipt", "Tempe Townhome", "Owner Statement"]
+GMAIL_ACCOUNT     = "berrystaysllc@gmail.com"
+REPORT_TO         = "berrystaysllc@gmail.com"
+AVALARA_PROPERTY  = "167558 - Rice Drive LLC"
+AIRBNB_TAX_PHRASE = "Taxes for this reservation already paid by Vendor"
